977b91375760754349da9221f9dce13126f8ca8d
xQsme <pedro-ferreira92@hotmail.com>
2020-08-12 13:25:56 +0100

    Initial commit

---
+++ precache-manifest.a97fc6752d1a735ecc675cfe2c97d73c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "756d282bd95ad96f0bcffafe3c743df4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a23e7dcd4b5eb67034e6",
+    "url": "/static/css/main.d4c3d5b5.chunk.css"
+  },
+  {
+    "revision": "30dc9b8b0523eeed0fda",
+    "url": "/static/js/2.f85188d7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.f85188d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a23e7dcd4b5eb67034e6",
+    "url": "/static/js/main.867abcd0.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  },
+  {
+    "revision": "317900f7617b309965dd9ca7bb74deab",
+    "url": "/static/media/back.317900f7.png"
+  },
+  {
+    "revision": "8c4c112223f4947b09d6eb08a9ecc4c0",
+    "url": "/static/media/front.8c4c1122.png"
+  },
+  {
+    "revision": "7d31f48dacbf15f21aea4aff5f2e561e",
+    "url": "/static/media/linux.7d31f48d.png"
+  },
+  {
+    "revision": "03a2fc79a455625959a7b5fea0245510",
+    "url": "/static/media/me.03a2fc79.jpg"
+  },
+  {
+    "revision": "c1eb7c08d65b0c19d2750980cceda76e",
+    "url": "/static/media/services.c1eb7c08.png"
+  }
+]);
